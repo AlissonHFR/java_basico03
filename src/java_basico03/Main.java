428a78bac37d4193221c2ed2194d9615017e46b5
Alisson <alissonhenrique98@gmail.com>
2020-03-04 19:52:38 -0300

    Commit final

--- src/java_basico03/Main.java
+++ src/java_basico03/Main.java
@@ -1,5 +1,7 @@
 package java_basico03;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,8 +12,20 @@ public class Main {
 	}
 
 	private void start() {
-		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
 		
+		System.out.println("Digite uma idade: ");
+		int idade = ler.nextInt();
+		
+		if(idade > 20) {
+			System.out.println("Idade maior que 20.");
+		}else if(idade <17 && idade > 12){
+			System.out.println("Idade menor que 17 e idade maior que 12");
+		}else if(idade == 7 || idade < 5) {
+			System.out.println("Idade igual a 7 ou idade menor que 5");
+		}else {
+			System.out.println("Outras combinações");
+		}
 	}
 
 }
